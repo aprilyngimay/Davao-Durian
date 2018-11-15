1e815bf8c75185c087d6ab75c6662081cd69087b
Kenneth Hautea <32695245+KennethHautea@users.noreply.github.com>
2018-11-15 17:26:28 +0800

    patch 5

---
+++ Davao-Durian-web/public/js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	var database = firebase.database();
+	var Switch;
+
+	database.ref('/arduinoDevice/').on('value', function(snap){
+		Switch = snap.val().Switch;
+		if(Switch == 1){
+			$("status").text("AUTO");
+		} else {
+			$("status").text("OFF")
+		}
+	});
+
+	$(".ledbtn").click(function(){
+		var firebaseRef = firebase.database().ref('/arduinoDevice/Switch');
+
+		if(Switch == 1){
+			firebaseRef.set(0);
+			Switch = 0;
+		} else {
+			firebaseRef.set(1);
+			Switch = 1;
+		}
+	});
+});
